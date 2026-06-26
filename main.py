a4d98033640cf4054fb412d8b8fe96f88ac164da
Hung <duc.pham@hs-osnabrueck.de>
2026-06-26 15:30:08 +0200

    Todo API miit GET Endpunkt hinzugefügt

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+todos = [
+    {"id": 1, "title": "Einkaufen", "done": False},
+    {"id": 2, "title": "8Kyu bearbeiten", "done": False}
+]
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return todo
+    raise HTTPException(status_code=404, detail="Todo not found")
